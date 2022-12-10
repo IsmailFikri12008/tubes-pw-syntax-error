a3659cde48f0d62ce689927f56e86ab911a1d42a
IsmailFikri12008 <203040008@mail.unpas.ac.id>
2022-12-10 22:09:03 +0700

    membuat setengah jadi halaman home dan routes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,52 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Test</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+@extends('layouts.main')
 
-</head>
-<body>
+@section('container')
 
-  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgba(5, 0, 36, 1)">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Syntax Error</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Beranda</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Rekomendasi</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Rangking Anime</a>
-          </li>
-        </ul>
-      </div>
+<section id="portfolio" class="portfolio">
+    <div class="container">
+        <div class="row mb-4 pt-4">
+            <div class="col text-center">
+                <h2>Top Anime</h2>
+            </div>
+        </div>
+
+        <div class="row mb-4">
+            <div class="col-md">
+                <div class="card">
+                    <img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="card">
+                    <img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="card">
+                    <img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="card">
+                    <img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </nav>
+</section>
 
-  <h1>Hello World!</h1>
-  <h1>Ini Home</h1>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
-</body>
-</html>
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,18 @@
+<footer>
+    <section id="footer" style="background-color: rgba(5, 0, 36, 1)">
+      <div class="container">
+        <div class="row" style="text-align:center;">
+          <div class="col">
+          </div>
+          <div class="col">
+
+        </div>
+        <p style="font-size: 20px; color: #fff; text-align:center; margin-top: 20px;">
+            Copyright &copy;
+            <script>
+                document.write(new Date().getFullYear());
+            </script> Syntax Error | All rights reserved
+        </p>
+      </div>
+    </section>
+  </footer>
