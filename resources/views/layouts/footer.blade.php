b570a7b64bc0332dbaa0520f9f74732a603f9195
FikriRidwan001 <203040001@mail.unpas.ac.id>
2022-12-16 00:48:22 +0700

    membuat footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,6 +7,30 @@
           <div class="col">
 
         </div>
+        <br>
+        <p class="text-start" style="color: #fff">Ikuti Kami:</p>
+        <div class="container">
+          <p  class="text-end" style="color: #fff; float:right; padding-top:10px;">
+            <a href="" style="text-decoration: none; color:#fff;">About</a> | <a href="" style="text-decoration: none; color:#fff;"> Login </a> | <a href="" style="text-decoration: none; color:#fff;"> Sign Up </a>
+          </p>
+          <div class="col text-start">
+              <a href="" style="text-decoration: none;">
+                <img src="../../../img/discord.png" alt="" style="height:50px; padding-left:10px; padding-right:10px;">
+              </a>
+              <a href="" style="text-decoration: none;">
+                <img src="../../../img/github.png" alt="" style="height:50px; padding-left:10px; padding-right:10px;">
+              </a>
+              <a href="" style="text-decoration: none;">
+                <img src="../../../img/instagram.png" alt="" style="height:50px; padding-left:10px; padding-right:10px;">
+              </a>
+              <a href="" style="text-decoration: none;">
+                <img src="../../../img/youtube.png" alt="" style="height:50px; padding-left:10px; padding-right:10px;">
+              </a>
+          </div>
+        </div>
+
+        <hr style="color: #fff; margin-top:15px;">
+
         <p style="font-size: 20px; color: #fff; text-align:center; margin-top: 20px;">
             Copyright &copy;
             <script>
