d0ed8f86e1ea560c56619240c9a8ff51cd1c3191
Salsabila Firdausi <113875646+Salsabyy@users.noreply.github.com>
2022-12-19 14:41:17 +0700

    membuat halaman ranking

--- resources/views/rangking.blade.php
+++ resources/views/rangking.blade.php
@@ -1,5 +1,79 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 style="margin-top: 80px;">Halaman Rangking</h1>
-@endsection
+    <h1 style="margin-top: 80px; text-align:center;">Halaman Rangking</h1>
+    <div class="row mb-4 pt-4"></div>
+    <div class="container">
+    <table class="table">
+        <thead>
+          <tr style="text-align: center">
+            <th scope="col">Rank</th>
+            <th scope="col">Judul </th>
+            <th scope="col">Skor</th>
+            <th scope="col">Gambar</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td style="text-align: center">Fullmetal Alchemist: Brotherhood</td>
+            <td>9.12</td>
+            <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td style="text-align: center">Bleach:Sennen Kessen-hen</td>
+            <td>9.10</td>
+            <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td style="text-align: center">Kaguya-sama wa Kokurasentai: Ultra Romantik</td>
+            <td>9.09</td>
+            <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+            </tr>
+        <tr>
+          <th scope="row">4</th>
+          <td style="text-align: center">Steins: Gate</td>
+          <td>9.08</td>
+          <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+        </tr>
+        <tr>
+        <th scope="row">5</th>
+        <td style="text-align: center">Gintama</td>
+        <td>9.07</td>
+        <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+      </tr>
+      <tr>
+        <th scope="row">6</th>
+        <td style="text-align: center">Shingeki no Kyojin Season 3 Part 2</td>
+        <td>9.06</td>
+        <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+      </tr>
+      <tr>
+      <th scope="row">7</th>
+      <td style="text-align: center">Gintama'</td>
+      <td>9.05</td>
+      <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td style="text-align: center">Gintama: The Final</td>
+      <td>9.05</td>
+      <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+    </tr>
+    <tr>
+    <th scope="row">9</th>
+    <td style="text-align: center">Gintama: Enchousen</td>
+    <td>9.04</td>
+    <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+  </tr>
+  <tr>
+    <th scope="row">10</th>
+    <td style="text-align: center">Hunter x Hunter (2011)</td>
+    <td>9.04</td>
+    <td><img src="https://i.pinimg.com/originals/bc/ac/b4/bcacb44ddb47d02a6ba78a6c8a990871.jpg" alt="" style="height: 250px;"></td>
+  </tr>
+        </tbody>
+      </table>
+    </div>
